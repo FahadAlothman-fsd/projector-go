566ebf1a3cda15982fdeb22335164f44defac38b
fAlothman <f.alothman@bimventures.com>
2023-05-12 23:00:22 +0300

    feat(config): add Projector object

--- cmd/projector/main.go
+++ cmd/projector/main.go
@@ -12,5 +12,11 @@ func main() {
 	if err != nil {
 		log.Fatalf("unable to get opts %v", err)
 	}
+	config, err := cli.NewConfig(opts)
+	if err != nil {
+		log.Fatalf("unable to get config %v\n", err)
+	}
+
 	fmt.Printf("opts: %+v\n", opts)
+	fmt.Printf("config: %+v\n", config)
 }
